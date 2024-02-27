17e248d8f29dcdd38b3f93bf1a8296c433f7a2fc
Kyle Vedder <kyle.c.vedder@gmail.com>
2024-02-27 17:45:01 -0500

    Temp commit of nuscenes stuff

--- launch.sh
+++ launch.sh
@@ -5,6 +5,7 @@ docker run --gpus=all --rm -it \
  --shm-size=16gb \
  -v `pwd`:/bucketed_scene_flow_eval \
  -v /efs:/efs \
+ -v /efs2:/efs2 \
  -v /tmp/.X11-unix:/tmp/.X11-unix \
  -v /tmp:/tmp \
  -v `pwd`/docker_history.txt:/root/.bash_history \
