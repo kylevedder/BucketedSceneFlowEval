304fd5b16d79039900114f40da37649e465f138f
Kyle Vedder <kyle.c.vedder@gmail.com>
2024-03-09 20:19:14 -0500

    Internals cleanup with partial Nuscenes support (#3)

--- bucketed_scene_flow_eval/datasets/shared_datastructures/abstract_sequence_loader.py
+++ bucketed_scene_flow_eval/datasets/shared_datastructures/abstract_sequence_loader.py
@@ -22,5 +22,27 @@ class AbstractSequenceLoader(ABC):
         pass
 
     @abstractmethod
+    def get_sequence_ids(self) -> list:
+        pass
+
+    @abstractmethod
     def load_sequence(self, sequence_identifier) -> AbstractSequence:
         pass
+
+
+class CachedSequenceLoader(AbstractSequenceLoader):
+    def __init__(self):
+        self.last_loaded_sequence = None
+        self.last_loaded_sequence_id = None
+
+    @abstractmethod
+    def _load_sequence_uncached(self, sequence_identifier) -> AbstractSequence:
+        pass
+
+    def load_sequence(self, sequence_identifier) -> AbstractSequence:
+        # Basic caching mechanism for repeated loads of the same sequence
+        if self.last_loaded_sequence_id != sequence_identifier:
+            self.last_loaded_sequence = self._load_sequence_uncached(sequence_identifier)
+            self.last_loaded_sequence_id = sequence_identifier
+
+        return self.last_loaded_sequence
--- tests/integration_tests.sh
+++ tests/integration_tests.sh
@@ -5,7 +5,7 @@
 
 # Prepare /tmp/argoverse2_tiny/val
 rm -rf /tmp/argoverse2_tiny
-wget https://github.com/kylevedder/BucketedSceneFlowEval/files/13881746/argoverse2_tiny.zip -O /tmp/argoverse2_tiny.zip
+wget -q https://github.com/kylevedder/BucketedSceneFlowEval/files/13881746/argoverse2_tiny.zip -O /tmp/argoverse2_tiny.zip
 unzip -q /tmp/argoverse2_tiny.zip -d /tmp/
 
 # For testing the raw data loader only (no flow) mode of the argoverse2 dataset, we symlink a "test" split to the val split
@@ -13,7 +13,14 @@ ln -s /tmp/argoverse2_tiny/val /tmp/argoverse2_tiny/test
 
 # Prepare /tmp/waymo_open_processed_flow_tiny
 rm -rf /tmp/waymo_open_processed_flow_tiny
-wget https://github.com/kylevedder/BucketedSceneFlowEval/files/13924555/waymo_open_processed_flow_tiny.zip -O /tmp/waymo_open_processed_flow_tiny.zip
+wget -q https://github.com/kylevedder/BucketedSceneFlowEval/files/13924555/waymo_open_processed_flow_tiny.zip -O /tmp/waymo_open_processed_flow_tiny.zip
 unzip -q /tmp/waymo_open_processed_flow_tiny.zip -d /tmp/
 
-pytest tests/integration_tests.py tests/eval/bucketed_epe.py tests/eval/threeway_epe.py
+
+# Prepare /tmp/nuscenes v1.0-mini
+rm -rf /tmp/nuscenes
+mkdir -p /tmp/nuscenes
+wget -q https://www.nuscenes.org/data/v1.0-mini.tgz -O /tmp/nuscenes/nuscenes_v1.0-mini.tgz
+tar -xzf /tmp/nuscenes/nuscenes_v1.0-mini.tgz -C /tmp/nuscenes
+
+pytest tests/integration_tests.py tests/eval/bucketed_epe.py tests/eval/threeway_epe.py tests/nuscenes/nuscenes_tests.py
